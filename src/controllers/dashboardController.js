f003d05bae8d2bce977a93437f9ef27b4c909d80
Arielbe555 <arielbe25@gmail.com>
2025-11-22 12:27:49 -0300

    fix: update dashboardController import

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,5 +1,5 @@
 // src/controllers/dashboardController.js
-import { pool } from "../database/db.js";
+import  pool  from "../database/db.js";
 
 /* ============================================================
    RESUMEN GENERAL DEL SISTEMA → PARA DASHBOARD
